e108d168ddfa842c21e17939197823855e878e7c
frontpage community <joshuasimon656@gmail.com>
2022-05-26 20:25:58 -0700

    HomePage creation

--- app/src/main/java/com/example/mtoto_app/Home.java
+++ app/src/main/java/com/example/mtoto_app/Home.java
@@ -1,14 +1,48 @@
 package com.example.mtoto_app;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
 
+    ValueLineChart lineChart;
+    ValueLineSeries series;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        lineChart = findViewById(R.id.lineChart);
+
+        lineChart.setLegendColor(getColor(R.color.white));
+        series = new ValueLineSeries();
+        series.setColor(getColor(R.color.white));
+
+        series.addPoint(new ValueLinePoint("Jan", 0));
+        series.addPoint(new ValueLinePoint("Feb", 7));
+        series.addPoint(new ValueLinePoint("Mar", 10));
+        series.addPoint(new ValueLinePoint("Apr", 3));
+        series.addPoint(new ValueLinePoint("Mai", 15));
+        series.addPoint(new ValueLinePoint("Jun", 20));
+        series.addPoint(new ValueLinePoint("Jul", 25));
+
+        lineChart.addSeries(series);
+        lineChart.startAnimation();
+
     }
 }
--- app/src/main/java/com/example/mtoto_app/MainActivity.java
+++ app/src/main/java/com/example/mtoto_app/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.mtoto_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView ingia_button,jisajiri_btn;
+    TextView ingia_button, jisajiri_btn;
+    EditText mtoto_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,16 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         ingia_button = findViewById(R.id.ingia_button);
         jisajiri_btn = findViewById(R.id.jisajiri_btn);
+        mtoto_number = findViewById(R.id.mtoto_number);
 
+        ingia_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mtoto_number.getText().toString().equals("")) {
+                    if (mtoto_number.getText().toString().equals("123")) {
+                        startActivity(new Intent(getApplicationContext(), Home.class));
+                        finish();
+                    }else {
+                        Toast.makeText(MainActivity.this, "Namba ya mtoto sio sahihi", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "Jaza namba ya mtoto", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         jisajiri_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,Jisajiri.class));
+                startActivity(new Intent(MainActivity.this, Jisajiri.class));
             }
         });
 
     }
+
 
     @Override
     public void onBackPressed() {
